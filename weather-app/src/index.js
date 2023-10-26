e5309cc6136dbb6d7515700f79c54d8d0fe16afa
evie-8 <neouma29@gmail.com>
2023-10-26 18:56:36 +0300

    remvoing preloader

--- weather-app/src/index.js
+++ weather-app/src/index.js
@@ -1,11 +1,6 @@
 const logo = document.querySelector(".logo img");
 const root = document.body;
 
-window.addEventListener("load", function () {
-let loader = document.querySelector(".loading");
-loader.style.display = "none";
-});
-
 logo.addEventListener("click", function () {
 if (root.classList.contains('light-mode')) {
     root.classList.remove('light-mode');
