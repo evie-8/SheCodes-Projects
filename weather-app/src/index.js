be1a7133ba1474111588afb7f3bf63d35b7f6d76
evie-8 <neouma29@gmail.com>
2023-10-26 22:14:26 +0300

    search button for mobile phones activated

--- weather-app/src/index.js
+++ weather-app/src/index.js
@@ -1,11 +1,13 @@
 const logo = document.querySelector(".logo img");
 const root = document.body;
 
+/* preloader */
 window.addEventListener("load", function () {
 let loader = document.querySelector(".loading");
 loader.classList.add("preloader--hidden");
 });
 
+/* dark theme effect */
 logo.addEventListener("click", function () {
 if (root.classList.contains('light-mode')) {
     root.classList.remove('light-mode');
@@ -14,4 +16,18 @@ if (root.classList.contains('light-mode')) {
   root.classList.add('light-mode');
   logo.src = "src/images/favicon - Copy.svg";
 }
+});
+
+
+/*search for mobiles */
+let searchView = document.querySelector("[data-search-view]");
+let searchToggle = document.querySelectorAll("[data-search-toggler]");
+
+console.log(searchToggle);
+function activate() {
+  searchView.classList.toggle("active");
+}
+
+searchToggle.forEach(function (element) {
+  element.addEventListener("click", activate);
 });
