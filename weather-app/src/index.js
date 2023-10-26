7ae778c2df3d39f01589a72e8f342334f67468b3
evie-8 <neouma29@gmail.com>
2023-10-26 11:27:00 +0300

    creating a fully functioning weather-app 1. creating the front end design using html and css

---
+++ weather-app/src/index.js
@@ -0,0 +1,11 @@
+const logo = document.querySelector(".logo img");
+const root = document.body;
+logo.addEventListener("click", function () {
+if (root.classList.contains('light-mode')) {
+    root.classList.remove('light-mode');
+    logo.src = "src/images/favicon.svg";
+} else {
+  root.classList.add('light-mode');
+  logo.src = "src/images/favicon - Copy.svg";
+}
+});
